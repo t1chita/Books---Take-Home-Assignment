ec035a67e7240749a5d86836cf9a462f5692003c
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-12-22 13:29:51 +0400

    initial xcode project

---
+++ BooksApp/BooksApp/BooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by Temur Chitashvili on 20.12.24.
+//
+
+import SwiftUI
+
+@main
+struct BooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
